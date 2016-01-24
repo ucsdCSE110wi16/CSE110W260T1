5cc98def75e9bd841c1621c246e3db4b66f28602
Ismail Alam <ismail.alam2@gmail.com>
2016-01-23 17:38:00 -0800

    Created a login and sign up created for app

---
+++ app/src/main/java/grouphub/travelshare/Signup.java
@@ -0,0 +1,115 @@
+package grouphub.travelshare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class Signup extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "Signup";
+    private EditText passwordField;
+    private EditText usernameField;
+    private EditText emailField;
+    private EditText confirmPasswordField;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        passwordField = (EditText) findViewById(R.id.passwordSignUp);
+        usernameField = (EditText) findViewById(R.id.username);
+        emailField = (EditText) findViewById(R.id.emailSignUp);
+        confirmPasswordField = (EditText) findViewById(R.id.confpasswordSignUp);
+
+
+        Button signup = (Button) findViewById(R.id.signupButton);
+
+        signup.setOnClickListener(this);
+
+    }
+
+    private boolean signUp() {
+        String password = passwordField.getText().toString();
+        String username =usernameField.getText().toString();
+        String email = emailField.getText().toString();
+        String confirmPassword = confirmPasswordField.getText().toString();
+
+        boolean errors = false;
+
+        if(password == "" || email == "" || username == "" || confirmPassword == "") {
+            errors = true;
+        }
+
+        if(!password.equals(confirmPassword)) {
+            errors = true;
+        }
+
+        //TODO: check email
+
+        ParseUser user = new ParseUser();
+       // user.setEmail(email);
+        user.setPassword(password);
+        user.setUsername(email);
+
+        user.put("screenName", username);
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e == null) {
+                    Toast.makeText(Signup.this, "SIGNED UP", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(Signup.this, e.getMessage().toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        return true;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        signUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_log_in, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/grouphub/travelshare/Start.java
+++ app/src/main/java/grouphub/travelshare/Start.java
@@ -1,44 +1,49 @@
 package grouphub.travelshare;
 
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.parse.LogInCallback;
-import com.parse.ParseException;
 import com.parse.ParseObject;
-import com.parse.ParseUser;
 
-public class Login extends AppCompatActivity implements View.OnClickListener {
+public class Start extends AppCompatActivity {
 
     private static final String TAG = "Login";
-    private Button login;
-    private EditText email;
-    private EditText password;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.start);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
-        Button login = (Button) findViewById(R.id.loginButton);
-        email = (EditText) findViewById(R.id.email);
-        password = (EditText) findViewById(R.id.password);
+        Log.d(TAG, "HELOOO");
 
-        login.setOnClickListener(this);
+        Button login = (Button) findViewById(R.id.login);
+        Button signup = (Button) findViewById(R.id.signup);
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startLogin = new Intent(Start.this, Login.class);
+                startActivity(startLogin);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUp =  new Intent(Start.this, Signup.class);
+                startActivity(signUp);
+            }
+        });
 
     }
 
@@ -47,27 +52,6 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_log_in, menu);
         return true;
-    }
-    private void login() {
-        String password = this.password.getText().toString();
-        String email = this.email.getText().toString();
-
-        ParseUser.logInInBackground(email, password, new LogInCallback() {
-            @Override
-            public void done(ParseUser user, ParseException e) {
-                if(user != null) {
-                    Toast.makeText(Login.this, "HORRAY YOU LOGINED IN", Toast.LENGTH_LONG).show();
-                }
-                else {
-                    Toast.makeText(Login.this, e.getMessage().toString(), Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-    }
-
-    @Override
-    public void onClick(View view) {
-        login();
     }
 
     @Override

692a3559ab5bcf0759fd354f3fb07a588ad133ff
Ismail Alam <ismail.alam2@gmail.com>
2016-02-07 00:55:47 -0800

    Added Daniel's buttons of goodness with barebone functionality (two of them can switch pages). More fragments have been made.

--- app/src/main/java/grouphub/travelshare/FoldersFragment.java
+++ app/src/main/java/grouphub/travelshare/FoldersFragment.java
@@ -3,7 +3,7 @@ package grouphub.travelshare;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Group.OnFragmentInteractionListener} interface
+ * {@link FoldersFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Group#newInstance} factory method to
+ * Use the {@link FoldersFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Group extends Fragment {
+public class FoldersFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class Group extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public Group() {
+    public FoldersFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class Group extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Group.
+     * @return A new instance of fragment FoldersFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Group newInstance(String param1, String param2) {
-        Group fragment = new Group();
+    public static FoldersFragment newInstance(String param1, String param2) {
+        FoldersFragment fragment = new FoldersFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class Group extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_group, container, false);
+        return inflater.inflate(R.layout.fragment_folders, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/grouphub/travelshare/ToolbarFragment.java
+++ app/src/main/java/grouphub/travelshare/ToolbarFragment.java
@@ -3,21 +3,20 @@ package grouphub.travelshare;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+public class ToolbarFragment extends Fragment implements View.OnClickListener{
+    View view;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ToolbarFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ToolbarFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ToolbarFragment extends Fragment {
+    private Button button_folders;
+    private Button button_manager;
+    private Button button_camera;
+    private Button button_views;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -26,8 +25,6 @@ public class ToolbarFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public ToolbarFragment() {
         // Required empty public constructor
@@ -63,46 +60,34 @@ public class ToolbarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_toolbar, container, false);
-    }
+        view = inflater.inflate(R.layout.fragment_toolbar, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        button_folders = (Button) view.findViewById(R.id.button_folders);
+        button_camera = (Button) view.findViewById(R.id.button_camera);
+        button_manager = (Button) view.findViewById(R.id.button_manager);
+        button_views = (Button) view.findViewById(R.id.button_views);
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+        button_folders.setOnClickListener(this);
+        button_camera.setOnClickListener(this);
+        button_manager.setOnClickListener(this);
+        button_views.setOnClickListener(this);
+
+        return view;
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_folders:
+                ((Main)getActivity()).switchToFolders();
+                break;
+            case R.id.button_camera:
+                break;
+            case R.id.button_manager:
+                ((Main)getActivity()).switchToGroupManager();
+                break;
+            case R.id.button_views:
+                break;
+        }
     }
 }

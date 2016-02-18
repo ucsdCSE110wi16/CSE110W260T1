c4e5dc83fffa82f501c1722a8772ee4dfb4dd617
chinowth <chinowth@gmail.com>
2016-02-17 21:19:44 -0800

    Logout test implemented as well as added library for assisting with testing.

---
+++ app/src/androidTest/java/grouphub/travelshare/LogoutTest.java
@@ -0,0 +1,69 @@
+package grouphub.travelshare;
+
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.support.test.rule.ActivityTestRule;
+        import android.support.test.runner.AndroidJUnit4;
+
+        import org.junit.Before;
+        import org.junit.Rule;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+
+        import static android.support.test.espresso.Espresso.onData;
+        import static android.support.test.espresso.Espresso.onView;
+        import static android.support.test.espresso.action.ViewActions.click;
+        import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+        import static android.support.test.espresso.action.ViewActions.typeText;
+        import static android.support.test.espresso.assertion.ViewAssertions.matches;
+        import static android.support.test.espresso.matcher.ViewMatchers.withId;
+        import static android.support.test.espresso.matcher.ViewMatchers.withText;
+        import static org.hamcrest.Matchers.allOf;
+        import static org.hamcrest.Matchers.anything;
+        import static org.hamcrest.Matchers.is;
+
+/**
+ * Created by danger on 2/17/2016.
+ * Given, we are logged in and on the homepage.
+ * Press the button in the top right to open drop down menu
+ * Press logout button
+ * result: logged out in the login screen
+ */
+@RunWith(AndroidJUnit4.class)
+public class LogoutTest {
+
+    @Rule
+    public ActivityTestRule<Main> activityTestRule =
+            new ActivityTestRule<>(Main.class);
+
+    @Test
+    public void LogoutTestCheck() {
+
+        try {
+            Thread.sleep(1000);
+        }catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.dropdown_menu)).perform(click());
+
+        try {
+            Thread.sleep(1000);
+        }catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onData(anything()).atPosition(1).perform(click());
+
+        try {
+            Thread.sleep(7000);
+        }catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        onView(withId(R.id.login_button)).check(matches(withId(R.id.login_button)));
+
+    }
+
+}
